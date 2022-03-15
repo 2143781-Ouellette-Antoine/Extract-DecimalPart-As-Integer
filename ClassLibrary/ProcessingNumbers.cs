603d57115dd360b222a3ea1c40f606df7bac9fdc
Antoine Ouellette <2143781@carrefour.cegepvicto.ca>
2022-03-14 23:16:56 -0400

    ClassLibrary: added the 3 first functions. ConsoleApp: added test code for explanation.

---
+++ ClassLibrary/ProcessingNumbers.cs
@@ -0,0 +1,41 @@
+﻿namespace ClassLibrary
+{
+    public class ProcessingNumbers
+    {
+        public static int TrouverNbrMaxDigits(decimal[] tableau)//Le maximum de digits apres la virgule
+        {
+            int nbrMax = NombreDigits(tableau[0]);
+            for (int currentIndex = 1; currentIndex <= tableau.Length - 1; currentIndex++)
+            {
+                int currentNombreDigits = NombreDigits(tableau[currentIndex]);
+                if (currentNombreDigits > nbrMax)
+                {
+                    nbrMax = currentNombreDigits;
+                }
+            }
+
+            return nbrMax;
+        }
+
+        public static int NombreDigits(decimal numero)//Nombre de digits dans la partie decimale
+        {
+            int count = 0;
+            //0.125
+            while (numero - Decimal.Truncate(numero) != 0)
+            {
+                numero *= 10;
+                count++;
+            }
+
+            return count;
+        }
+
+        public static int TransformerEnInt(decimal numero, int maxDigits)
+        {
+            //Tronquer partie entiere
+            decimal partieDecimale = numero - (int)Math.Truncate(numero);
+            //Shifter les chiffres a gauche de la virgule, pour un longueur de maxDigits.
+            return (int)(partieDecimale * (decimal)Math.Pow(10, maxDigits));
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using ClassLibrary;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //Trouver nombre de digits apres la virgule d'un numero
+        decimal numero1 = 2.125M;
+        Console.WriteLine(ProcessingNumbers.NombreDigits(numero1));
+
+        //Trouver le -nombre maximal de digits apres la virgule- d'un tableau
+        decimal[] tableau = { 2.125M, 2.2M, 2.231M, 0.515M, 0.005M, 0.6M };
+        int maxDigits = ProcessingNumbers.TrouverNbrMaxDigits(tableau);
+        Console.WriteLine(maxDigits);
+
+        //Extraire la partie decimale d'un nombre et
+        //la Transformer en Entier(Int)
+        decimal numero2 = 2.2M;
+        Console.WriteLine(ProcessingNumbers.TransformerEnInt(numero2, maxDigits));
+        //______________________
+        //| Entiers | Decimaux |    0.125 => 125
+        //|      2. | 125      |    0.125  x  10^3 = 125
+        //|      0. | 005      |
+        //|      2. | 200      |    numero *  10^{maxDigits} = PartieDecimaleEnInt
+        //|_________|__________|    On deplace les decimaux a gauche de la virgule
+
+        Console.ReadKey(true);
+    }
+}
